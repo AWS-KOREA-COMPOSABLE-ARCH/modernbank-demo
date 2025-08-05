dbdbb2e811cdc938477db14dc7de3126e2684135
Han, Sanghoon <hanshoon@amazon.com>
2025-08-05 06:56:50 +0000

    modify deploy

--- k8s/apply.sh
+++ k8s/apply.sh
@@ -4,6 +4,6 @@ kubectl apply -n modernbank -f account/
 kubectl apply -n modernbank -f b2bt/
 kubectl apply -n modernbank -f customer/
 kubectl apply -n modernbank -f cqrs/
-kubectl apply -n modernbank -f product/
+#kubectl apply -n modernbank -f product/
 kubectl apply -n modernbank -f transfer/
 kubectl apply -n modernbank -f user/
